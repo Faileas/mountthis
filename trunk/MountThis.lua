23c51aad623603387dd84cc16046178bde0aaf7a
govtgeek <govtgeek@9f922fd2-abb9-11dd-9b86-41075523de00>
2010-10-14 03:08:39 +0000

    Updated the version number *sigh*

--- trunk/MountThis.lua
+++ trunk/MountThis.lua
@@ -9,7 +9,7 @@ BINDING_HEADER_MOUNTTHIS = "MountThis";
 BINDING_NAME_MOUNTRANDOM = "Random Mount";
 
 MountThis = LibStub("AceAddon-3.0"):NewAddon("MountThis", "AceConsole-3.0", "AceComm-3.0", "AceEvent-3.0");
-MountThis.version = 0.96;
+MountThis.version = 0.97;
 MountThis.reqVersion = MountThis.version;
 MountThis.optionsFrames = {};
 MountThisSettings =
