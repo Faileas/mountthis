ddfc0b1445c17aec466247862823cc26b8551f7d
govtgeek <govtgeek@9f922fd2-abb9-11dd-9b86-41075523de00>
2011-08-28 06:49:35 +0000

    Fix for the "new mount doesn't get a checkbox" error?

---
+++ tags/version-4.2.0.002/Localization/localization.kr.lua
@@ -0,0 +1,61 @@
+if GetLocale() ~= "koKR" then return end
+
+BINDING_HEADER_MOUNTTHIS = "MountThis";
+BINDING_NAME_MOUNTRANDOM = "Random Mount";
+
+MOUNTTHIS_OPTION_HEADER = "MountThis"
+
+MOUNTTHIS_OPTION_LIST = "List"
+MOUNTTHIS_OPTION_LIST_DESC = "List all mounts known to MountThis"
+
+MOUNTTHIS_OPTION_FORCE_UPDATE = "Update"
+MOUNTTHIS_OPTION_FORCE_UPDATE_DESC = "Force update all mounts"
+
+MOUNTTHIS_OPTION_SHOW_VERSION = "Version"
+MOUNTTHIS_OPTION_SHOW_VERSION_DESC = "Show MountThis version"
+MOUNTTHIS_OPTION_VERSION_STRING = "You are using MountThis version"
+
+MOUNTTHIS_OPTION_DISMOUNT_IF_MOUNTED = "Dismount"
+MOUNTTHIS_OPTION_DISMOUNT_IF_MOUNTED_DESC = "Make MountThis dismount if used while mounted instead of attempting to remount"
+
+MOUNTTHIS_OPTION_EXIT_VEHICLE = "Exit vehicle"
+MOUNTTHIS_OPTION_EXIT_VEHICLE_DESC = "Make MountThis dismount if used while in a vehicle"
+
+MOUNTTHIS_OPTION_DONT_USE_LAST_MOUNT = "Don't use last mount"
+MOUNTTHIS_OPTION_DONT_USE_LAST_MOUNT_DESC = "MountThis will not use the last mount used"
+
+MOUNTTHIS_OPTION_MOUNT_LAND = "Use modifier for non-flying mount in flyable zone"
+MOUNTTHIS_OPTION_MOUNT_LAND_DESC = "Use a modifier to make MountThis use a non-flying mount when a flyable zone"
+MOUNTTHIS_OPTION_MOUNT_LAND_KEY = "Land mount modifier"
+MOUNTTHIS_ALT = "Alt"
+MOUNTTHIS_SHIFT = "Shift"
+MOUNTTHIS_CONTROL = "Control"
+
+MOUNTTHIS_OPTION_OPEN_CONFIG = "Options"
+MOUNTTHIS_OPTION_OPEN_CONFIG_DESC = "Open configuration options window"
+MOUNTTHIS_OPTION_OPEN_CONFIG_DEBUG_DESC = "Open debug configuration options window"
+
+MOUNTTHIS_OPTION_DEBUG_HEADER = "Debugging"
+MOUNTTHIS_OPTION_DEBUG_LEVEL = "Debug level"
+MOUNTTHIS_OPTION_DEBUG_LEVEL_DESC = "Set debugging output level"
+
+MOUNTTHIS_OPTION_CHATFRAME = "Debug output to chat frame #"
+MOUNTTHIS_OPTION_CHATFRAME_DESC = "Select which chat frame you want to send debug output"
+
+MOUNTTHIS_OPTION_CHATFRAME_IDENTIFY = "Identify chat frames"
+MOUNTTHIS_OPTION_CHATFRAME_IDENTIFY_DESC = "Show chat frame number in each chat frame"
+MOUNTTHIS_OPTION_CHATFRAME_IDENTIFY_STRING = "This is chatFrame"
+
+MOUNTTHIS_OPTION_MOUNTS_HEADER = "Mounts"
+
+MOUNTTHIS_LIST_MOUNTS_STRING = "Mount List"
+MOUNTTHIS_LIST_USE_MOUNT = "use"
+MOUNTTHIS_LIST_FLYING = "fly"
+MOUNTTHIS_LIST_PROFESSION = "prof"
+MOUNTTHIS_LIST_PASSENGERS = "pass"
+
+MOUNTTHIS_WINTERGRASP = "Wintergrasp"
+MOUNTTHIS_AHNQIRAJ = "Temple of Ahn'Qiraj"
+MOUNTTHIS_VASHJIR = "Vashj'ir"
+MOUNTTHIS_COLD_WEATHER_FLYING = "Cold Weather Flying"
+MOUNTTHIS_FLIGHT_MASTERS_LICENSE = "Flight Master's License"
